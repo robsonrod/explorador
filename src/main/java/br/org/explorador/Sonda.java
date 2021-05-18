1292597fa1b7b346374af6e4f138cfda93528b29
Robson Rodrigues <robsonrod@gmail.com>
2021-05-18 13:05:21 -0300

    Remover if e utilizar switch no metodo mover

--- src/main/java/br/org/explorador/Sonda.java
+++ src/main/java/br/org/explorador/Sonda.java
@@ -24,34 +24,43 @@ public class Sonda {
 
     public void mover() {
 
-        if (posicaoCorrente.getDirecao() == PontoCardeal.N) {
-            if (this.posicaoCorrente.getCoordenadaY() >= this.planalto.getAltura()) {
-                this.posicaoCorrente = this.posicaoInicial;
-                throw new IllegalStateException("Sonda em estado invalido. Tentativa de acessar um local inacessivel.");
+        switch (posicaoCorrente.getDirecao()) {
+            case N: {
+                if (this.posicaoCorrente.getCoordenadaY() >= this.planalto.getAltura()) {
+                    this.posicaoCorrente = this.posicaoInicial;
+                    throw new IllegalStateException("Sonda em estado invalido. Tentativa de acessar um local inacessivel.");
+                }
+                this.posicaoCorrente.moverDirecaoNorte();
             }
-            this.posicaoCorrente.moverDirecaoNorte();
+            break;
 
-        } else if (posicaoCorrente.getDirecao() == PontoCardeal.S) {
-            if (this.posicaoCorrente.getCoordenadaY() <= 0) {
-                this.posicaoCorrente = this.posicaoInicial;
-                throw new IllegalStateException("Sonda em estado invalido. Tentativa de acessar um local inacessivel.");
+            case S: {
+
+                if (this.posicaoCorrente.getCoordenadaY() <= 0) {
+                    this.posicaoCorrente = this.posicaoInicial;
+                    throw new IllegalStateException("Sonda em estado invalido. Tentativa de acessar um local inacessivel.");
+                }
+                this.posicaoCorrente.moverDirecaoSul();
             }
+            break;
 
-            this.posicaoCorrente.moverDirecaoSul();
-
-        } else if (posicaoCorrente.getDirecao() == PontoCardeal.E) {
-
-            if (this.posicaoCorrente.getCoordenadaX() >= this.planalto.getLargura()) {
-                this.posicaoCorrente = this.posicaoInicial;
-                throw new IllegalStateException("Sonda em estado invalido. Tentativa de acessar um local inacessivel.");
+            case E: {
+                if (this.posicaoCorrente.getCoordenadaX() >= this.planalto.getLargura()) {
+                    this.posicaoCorrente = this.posicaoInicial;
+                    throw new IllegalStateException("Sonda em estado invalido. Tentativa de acessar um local inacessivel.");
+                }
+                this.posicaoCorrente.moverDirecaoLeste();
             }
-            this.posicaoCorrente.moverDirecaoLeste();
-        } else {
-            if (this.posicaoCorrente.getCoordenadaX() <= 0) {
-                this.posicaoCorrente = this.posicaoInicial;
-                throw new IllegalStateException("Sonda em estado invalido. Tentativa de acessar um local inacessivel.");
+            break;
+
+            case W: {
+                if (this.posicaoCorrente.getCoordenadaX() <= 0) {
+                    this.posicaoCorrente = this.posicaoInicial;
+                    throw new IllegalStateException("Sonda em estado invalido. Tentativa de acessar um local inacessivel.");
+                }
+                this.posicaoCorrente.moverDirecaoOeste();
             }
-            this.posicaoCorrente.moverDirecaoOeste();
+            break;
         }
     }
 
